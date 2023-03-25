47a6fd0e2885ee84a0bf00c4cdb81bdee2d4a797
Yagafarov <dinmuhammadyagafarov@gmail.com>
2023-03-25 12:40:57 +0300

    Xatolik to'g'irladim

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Ro'yxatdan o'tish</title>
     <link rel="stylesheet" href="./assets/fonts/stylesheet.css">
     <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./styles/style.css">
@@ -14,9 +14,11 @@
 <body class="h-100">
 
     <!-- ########################## -->
-    <h1 class="text-center">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti quibusdam similique corporis corrupti consectetur quae eum, at ea eveniet itaque minus, iusto obcaecati beatae ducimus necessitatibus ipsum iste aliquid quidem.
-    </h1>
+    <section>
+      <h3 class="text-center">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, cum.
+      </h3>
+    </section>
     <!-- ########################## -->
 
     <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
